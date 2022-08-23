e555c7ee2d7299ab15ae25f7db2a52486148bfd7
Aditya Rana <adityarana@juniper.net>
2022-08-23 15:49:25 +0530

    Wrapping functions inside class

--- bot_core/bot.py
+++ bot_core/bot.py
@@ -3,8 +3,8 @@ from bot_core.utils import (
     Error_Handler,
     Response_Handler,
     Cred_Ops,
-    post_message,
-    fetch_marvis_response
+    Mist_Api,
+    post_message
 )
 import json
 
@@ -13,20 +13,6 @@ class BOT_PROCESSOR(ActivityHandler):
         text = text.strip()
         cleaned_text = text.replace("<at>Marvis-test</at>", "").strip()
         return cleaned_text
-
-    def _get_request_metadata(self, turn_context: TurnContext, org_id):
-        metadata = {
-            "time_zone": turn_context.activity.local_timezone,
-            "utc_timestamp": str(turn_context.activity.timestamp),
-            "local_timestamp": str(turn_context.activity.local_timestamp),
-            "first_name": turn_context.activity.from_property.name.split()[0],
-            "user_id": turn_context.activity.from_property.id,
-            "conversation_type": turn_context.activity.conversation.conversation_type,
-            "conversation_id": turn_context.activity.conversation.id,
-            "org_id": org_id,
-            "org_user": "True"
-        }
-        return metadata
 
     async def on_message_activity(self, turn_context: TurnContext):
         credentials = Cred_Ops(turn_context)
@@ -46,8 +32,8 @@ class BOT_PROCESSOR(ActivityHandler):
         # verify credentials
         if not await credentials.verify_credentials(token, org): return
 
-        request_metadata = self._get_request_metadata(turn_context, org)
-        api_response = fetch_marvis_response(user_msg, token, org, request_metadata)
+        mist_api = Mist_Api(turn_context, user_msg, token, org)
+        api_response = mist_api.fetch_marvis_response()
 
         # handling error response code
         if api_response.status_code != 200:
